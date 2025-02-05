3a6d024734d03dec00de58725e309666b50300a5
Urooj Sadiq <uroojsadiq1998@gmail.com>
2025-02-06 02:20:10 +0500

    build error fix

--- src/app/brands/page.tsx
+++ src/app/brands/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { motion } from "framer-motion";
+import TopPagepath from "@/components/top-pagepath";
 
 const brands = [
   { src: "/versacebrand1.png", alt: "brand1" },
@@ -9,29 +10,48 @@ const brands = [
   { src: "/calvinkleinbrand5.png", alt: "brand5" },
 ];
 
-  
 export default function Brands() {
   const itemVariants = {
     hidden: { opacity: 0, y: 30 },
     visible: { opacity: 1, y: 0, transition: { duration: 0.6 } },
   };
+
+  const paths = [
+    { label: "Home", href: "/" },
+    { label: "Brands", href: "" },
+  ];
   return (
     <main className="min-h-screen md:pt-28 pt-28 md:px-12 px-4 flex flex-col ">
-       <section className="bg-black p-6">
-      <div className="md:flex md:flex-row justify-center grid grid-cols-3 lg:gap-24 gap-10">
-        {brands.map((i, index) => (
-          <motion.div
-            key={index}
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: true, amount: 0.3 }}
-            variants={itemVariants}
-          >
-            <Image src={i.src} alt={i.alt} width={120} height={120} />
-          </motion.div>
-        ))}
+      <div>
+        <TopPagepath items={paths} />
       </div>
-    </section>
+      <motion.h1
+        initial="hidden"
+        animate="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        variants={{
+          hidden: { opacity: 0, scale: 0.8 },
+          visible: { opacity: 1, scale: 1, transition: { duration: 0.8 } },
+        }}
+        className="my-4 font-integral text-2xl font-extrabold"
+      >
+        Our Exclusive Brands
+      </motion.h1>
+      <section className="bg-black p-6">
+        <div className="md:flex md:flex-row justify-center grid grid-cols-3 lg:gap-24 gap-10">
+          {brands.map((i, index) => (
+            <motion.div
+              key={index}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.3 }}
+              variants={itemVariants}
+            >
+              <Image src={i.src} alt={i.alt} width={120} height={120} />
+            </motion.div>
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
